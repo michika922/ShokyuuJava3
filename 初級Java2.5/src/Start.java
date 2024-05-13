568500e68751a476fc5bf409db7919fe8441ac09
takeda michika <mi.chi.ka10@icloud.com>
2024-05-13 11:53:20 +0900

    gitignoreコミット

---
+++ 初級Java2.5/src/Start.java
@@ -0,0 +1,18 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+			MyFrame frame3=new RectFrame();
+			frame3.setLocation(50,300);
+			MyFrame frame4=new RectFrame();
+			frame3.setLocation(50,300);
+			
+			
+			int x = 2;
+			int y = 3;
+			int ans = x*y;
+			System.out.println(ans);
+	}
+
+}
