8b1ae36f19b1809e5993d45b6493ff9e48d8502a
takeda michika <mi.chi.ka10@icloud.com>
2024-05-14 12:10:57 +0900

    3.3さまざまな色で描く

--- 初級Java2.5/src/src/RectTest.java
+++ 初級Java2.5/src/src/RectTest.java
@@ -2,10 +2,16 @@ package src;
 
 public class RectTest extends MyFrame{
 	public void run() {
+		setColor(128,128,128);
 		fillRect(30,40,50,100);
+		setColor(0,128,0);
 		fillRect(130,40,50,100);
+		setColor(128,0,128);
 		fillRect(30,200,150,10);
-		
+		setColor(0,255,255);
+		fillRect(35,100,250,10);
+		setColor(0,0,0);
+		fillRect(40,200,150,40);
 	}
 		
 	
