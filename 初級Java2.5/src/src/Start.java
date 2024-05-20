dcd5a37abfcdd939f5195caaa393054d03bcf3d4
takeda michika <mi.chi.ka10@icloud.com>
2024-05-20 10:19:50 +0900

    練習問題3-1

--- 初級Java2.5/src/src/Start.java
+++ 初級Java2.5/src/src/Start.java
@@ -5,7 +5,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 			System.out.println("プログラムを実行します");
-			MyFrame frame1=new TextText();
+			MyFrame frame1=new OverTest();
 	}
 
 }
