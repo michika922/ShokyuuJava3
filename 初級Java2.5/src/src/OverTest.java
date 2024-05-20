d0cb526d5b18ac215dc55323d57a556f76d92937
takeda michika <mi.chi.ka10@icloud.com>
2024-05-20 10:52:59 +0900

    4.1図形をたくさん描く

--- 初級Java2.5/src/src/OverTest.java
+++ 初級Java2.5/src/src/OverTest.java
@@ -2,9 +2,10 @@ package src;
 
 public class OverTest extends MyFrame {
 	public void run() {
-		fillOval(30,40,50,100);
-		fillOval(100,40,50,100);
-		
+		fillRect(50,50,40,20);
+		fillRect(30,70,80,20);
+		fillOval(40 ,90,20,20);
+		fillOval(80,90,20,20);
 	}
 
 }
