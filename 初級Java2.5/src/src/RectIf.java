0d3895645d8bb62f89c6b68f2d5752ae40c5cff0
takeda michika <mi.chi.ka10@icloud.com>
2024-05-21 09:53:06 +0900

    practice4.3 P84

--- 初級Java2.5/src/src/RectIf.java
+++ 初級Java2.5/src/src/RectIf.java
@@ -3,12 +3,14 @@ package src;
 public class RectIf extends MyFrame {
 	public void run() {
 		int x=30;
-		setColor(0,128,0);
-		int y=10;
+		int y=0;
+		
 		while(x<180) {
-			fillRect(x,80,10,y);
+			fillRect(x,80,10,100);
+			setColor(y,y,y);
 			x+=20;
 			y+=20;
+			
 		}
 		
 		
