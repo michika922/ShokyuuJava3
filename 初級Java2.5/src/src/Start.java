5f393a8af8e13740ec85d67ba34a8536d9dabeb8
takeda michika <mi.chi.ka10@icloud.com>
2024-05-28 11:58:41 +0900

    7.2配列の利用

--- 初級Java2.5/src/src/Start.java
+++ 初級Java2.5/src/src/Start.java
@@ -5,11 +5,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 			System.out.println("プログラムを実行します");
-<<<<<<< Updated upstream
 			MyFrame frame1= new DisplayFaceAndTree();
-=======
-			MyFrame frame1= new DisplayFaceObject();
->>>>>>> Stashed changes
 	}
 
 }
