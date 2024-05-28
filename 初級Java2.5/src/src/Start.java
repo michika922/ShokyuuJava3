e2c0be534094f8842655f50ec6b58f15c7469fc7
takeda michika <mi.chi.ka10@icloud.com>
2024-05-28 11:52:19 +0900

    7.1前章の復習

--- 初級Java2.5/src/src/Start.java
+++ 初級Java2.5/src/src/Start.java
@@ -5,7 +5,11 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 			System.out.println("プログラムを実行します");
+<<<<<<< Updated upstream
 			MyFrame frame1= new DisplayFaceAndTree();
+=======
+			MyFrame frame1= new DisplayFaceObject();
+>>>>>>> Stashed changes
 	}
 
 }
