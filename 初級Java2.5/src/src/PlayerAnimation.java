17d1ced81fb3dfc73d021f19e5f188b1c3081a0f
takeda michika <mi.chi.ka10@icloud.com>
2024-06-10 18:45:46 +0900

    9.2キーボードに反応して動くようにする

--- 初級Java2.5/src/src/PlayerAnimation.java
+++ 初級Java2.5/src/src/PlayerAnimation.java
@@ -3,6 +3,7 @@ package src;
 public class PlayerAnimation extends MyFrame {
 	public void run() {
 		Car player = new Car(100,200,0,0);
+		addKeyListener(player);
 		while(true) {
 			clear();
 			player.draw(this);
